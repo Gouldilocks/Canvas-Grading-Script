4b7f9cb539f24cb4d1effa71a7e93be0470e3865
Christian Gould <cagould@smu.edu>
2021-04-01 17:52:54 -0500

    edits

--- myscript.sh
+++ myscript.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
-for dire in ./Data/*/; do
+for dire in $(find ./Data/ -mindepth 1 -maxdepth 1 -type d); do
+#for dire in ./Data/*/; do
 	echo "student dir: $dire"
+	#cd $(realpath -s $dire)
 	for filename in $(realpath -s $dire)/*.java; do
+	#for filename in *.java; do
 		echo "trimmed: $(realpath -s $filename)"
 		echo "compiling $(realpath -s $filename)" >> output.txt
-		javac $(realpath -s $filename)
+		javac $(realpath -s $filename) >> output.txt
 		echo "
 		      ***********
 		      ***********
 		      RUNNING
 		      ***********
 		      ***********"
-		      #script.exp >> output.txt
-		java $(realpath -s $filename) < ass.txt >> output.txt
+		      /home/christian/Desktop/JavaTASpring2021/scripting/javaScripting $(realpath -s $filename)
    
 	done
+	cd ..
 done
 
 	
