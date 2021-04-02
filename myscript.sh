dc54e1f214300b7f49ff1f8626c38116979043f5
Gouldilocks <cagould@smu.edu>
2021-04-01 19:56:12 -0500

    finished script

--- myscript.sh
+++ myscript.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 for dire in $(find ./Data/ -mindepth 1 -maxdepth 1 -type d); do
-#for dire in ./Data/*/; do
-	echo "student dir: $dire"
-	#cd $(realpath -s $dire)
-	for filename in $(realpath -s $dire)/*.java; do
-	#for filename in *.java; do
-		echo "trimmed: $(realpath -s $filename)"
+	fullDir=$PWD
+  	cd $(realpath -s $dire)
+	for filename in $(ls *.java); do
 		echo "compiling $(realpath -s $filename)" >> output.txt
 		javac $(realpath -s $filename) >> output.txt
 		echo "
 		      ***********
 		      ***********
-		      RUNNING
+		      RUNNING $dire
 		      ***********
 		      ***********"
-		      /home/christian/Desktop/JavaTASpring2021/scripting/javaScripting $(realpath -s $filename)
-   
+			y=${filename%.java}  
+			$fullDir/script.exp $y >> output.txt 
 	done
+	cd ..
 	cd ..
 done
 
